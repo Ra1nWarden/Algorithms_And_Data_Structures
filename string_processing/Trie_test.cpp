e9ef905452efec66af62de52a837a484a0479413
Ra1nWarden <wzh19921016@gmail.com>
2015-03-08 14:48:46 -0400

    Added more tests for Trie

--- string_processing/Trie_test.cpp
+++ string_processing/Trie_test.cpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-Trie trie;
-TrieChildBrother triecb;
-
 char word1[] = "cat";
 char word2[] = "case";
 char word3[] = "dog";
@@ -13,11 +10,13 @@ char word4[] = "abcdefghijklmnopqrstuvwxyz";
 char word5[] = "abcdefghijkl";
 
 TEST(Trie, InsertWord) {
+  Trie trie;
   trie.insert(word1, 1);
   EXPECT_EQ(1, trie.find(word1));
 }
 
 TEST(Trie, InsertWords) {
+  Trie trie;
   trie.insert(word1, 1);
   trie.insert(word2, 2);
   EXPECT_EQ(1, trie.find(word1));
@@ -25,13 +24,22 @@ TEST(Trie, InsertWords) {
 }
 
 TEST(Trie, InsertPrefix) {
+  Trie trie;
   trie.insert(word4, 100);
   trie.insert(word5, 1);
   EXPECT_EQ(1, trie.find(word5));
   EXPECT_EQ(100, trie.find(word4));
 }
 
+TEST(Trie, FindFailure) {
+  Trie trie;
+  trie.insert(word4, 100);
+  EXPECT_EQ(0, trie.find(word5));
+  EXPECT_EQ(100, trie.find(word4));
+}
+
 TEST(Trie, Remove) {
+  Trie trie;
   trie.insert(word1, 1);
   trie.insert(word2, 2);
   trie.del(word1);
@@ -40,11 +48,13 @@ TEST(Trie, Remove) {
 }
 
 TEST(TrieChildBrother, InsertWord) {
+  TrieChildBrother triecb;
   triecb.insert(word1, 1);
   EXPECT_EQ(1, triecb.find(word1));
 }
 
 TEST(TrieChildBrother, InsertWords) {
+  TrieChildBrother triecb;
   triecb.insert(word1, 1);
   triecb.insert(word2, 2);
   EXPECT_EQ(1, triecb.find(word1));
@@ -52,13 +62,22 @@ TEST(TrieChildBrother, InsertWords) {
 }
 
 TEST(TrieChildBrother, InsertPrefix) {
+  TrieChildBrother triecb;
   triecb.insert(word4, 100);
   triecb.insert(word5, 1);
   EXPECT_EQ(1, triecb.find(word5));
   EXPECT_EQ(100, triecb.find(word4));
 }
 
+TEST(TrieChildBrother, FindFailure) {
+  TrieChildBrother triecb;
+  triecb.insert(word4, 100);
+  EXPECT_EQ(0, triecb.find(word5));
+  EXPECT_EQ(100, triecb.find(word4));
+}
+
 TEST(TrieChildBrother, Remove) {
+  TrieChildBrother triecb;
   triecb.insert(word1, 1);
   triecb.insert(word2, 2);
   triecb.del(word1);
