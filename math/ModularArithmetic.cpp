c585d695ced0bbf44b23783a42fa16751ec00f59
Ra1nWarden <wzh19921016@gmail.com>
2015-11-19 21:29:35 -0500

    Changed power modular algorithm to iterative version

--- math/ModularArithmetic.cpp
+++ math/ModularArithmetic.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 
 int PowerModular(int a, int n, int m) {
-  if(n == 0)
-    return 1;
-  int x = PowerModular(a , n / 2, m);
-  long long ans = (long long) x * x % m;
-  if(n % 2 == 1)
-    ans = ans * a % m;
-  return (int) ans;
+  int ans = 1;
+  while(n) {
+    if(n & 1) {
+      ans = (ans * a) % m;
+    }
+    a = (a * a) % m;
+    n >>= 1;
+  }
+  return ans;
 }
 
 bool SolveModularEquation(int a, int b, int n, int& x) {
